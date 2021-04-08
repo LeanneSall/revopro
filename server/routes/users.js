99d5f12bb6632a1d0923257644dd981451dd5909
Leanne Sall <lsall1@my.bcit.ca>
2021-04-08 10:26:11 -0700

    preliminary user register w/ checks

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const User = require('../models/User');
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
 
 
-router.post('/', (req, res) => {
-    res.send('get user info')
+router.post('/', [
+    check('firstName', 'name is required').not().isEmpty(),
+    check('lastName', 'name is required').not().isEmpty(),
+    check('email', 'valid email is required').isEmail(),
+    check('password', 'password with 6 or more characters is required').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { firstName, lastName, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const encryptedPass = await bcrypt.hash(password, salt);
+
+        user = new User({
+            firstName,
+            lastName,
+            email,
+            encryptedPass
+        })
+
+        await user.save();
+
+        res.send('User saved into database');
+
+    } catch (e) {
+
+    }
+
 });
 
 
