458118cbd0a5e4622c89669f6cf53719b8d804d7
Leanne Sall <lsall1@my.bcit.ca>
2021-04-08 07:59:53 -0700

    added user model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const User = require('../models/User');
+
 const router = express.Router();
+
+
 
 router.post('/', (req, res) => {
     res.send('get user info')
